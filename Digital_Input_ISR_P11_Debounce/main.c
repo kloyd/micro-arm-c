dc893034f3387e66b931a0b08ad3b0017f1f6fb9
kloyd <kloyd@dmp.com>
2018-02-16 17:00:27 -0600

    Digital Inputs by Interrupt

---
+++ Digital_Input_ISR_P11_Debounce/main.c
@@ -0,0 +1,37 @@
+#include <msp430.h>
+
+#define ENABLE_PINS 0xFFFE // Required to use inputs and outputs
+
+void main(void) {
+    WDTCTL = WDTPW | WDTHOLD;  // Stop WDT
+
+    PM5CTL0 = ENABLE_PINS;  // Enable inputs and outputs
+
+    P1DIR = BIT0;  // P1.0 will be output for red LED
+
+    P1OUT = BIT1;  // P1.1 will be input with a pull-up
+    P1REN = BIT1;  // resistor. Additionally, the red LED will initially be off.
+
+    P1IE = BIT1;  // Enable interrupt for P1.1
+    P1IES = BIT1;  // for transitions from HI-->LO
+
+    P1IFG = 0x00;  // Ensure no ISRs are pending
+
+    _BIS_SR(LPM0_bits | GIE);  // Activate all interrupts
+
+    while (1)
+        ;  // Infinite loop
+}
+//***********************************************************************
+//* Port 1 Interrupt Service Routine
+//***********************************************************************
+#pragma vector=PORT1_VECTOR
+__interrupt void Port_1(void) {
+
+    unsigned long delay;
+    for(delay=0; delay<20000; delay++);
+
+    P1OUT = P1OUT ^ BIT0;  // Toggle the red LED with every button push
+
+    P1IFG &= ~(BIT1);  // Clear P1.1 interrupt flag
+}
